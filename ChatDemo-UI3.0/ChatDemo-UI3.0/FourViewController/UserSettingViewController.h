9da606211cf24fff00a1148cfa447e7645afec5e
maibou8888 <1520082546@qq.com>
2016-03-15 09:26:52 +0800

    JN_XMTT

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/FourViewController/UserSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserSettingViewController.h
+//  ChatDemo-UI3.0
+//
+//  Created by 王明哲 on 16/3/11.
+//  Copyright © 2016年 王明哲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserSettingViewController : UIViewController
+
+@end
---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/FourViewController/UserSettingViewController.m
@@ -0,0 +1,37 @@
+//
+//  UserSettingViewController.m
+//  ChatDemo-UI3.0
+//
+//  Created by 王明哲 on 16/3/11.
+//  Copyright © 2016年 王明哲. All rights reserved.
+//
+
+#import "UserSettingViewController.h"
+
+@interface UserSettingViewController ()
+
+@end
+
+@implementation UserSettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
